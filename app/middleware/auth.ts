4e052a5777a5401cfa15cc6cb07a3c2c73fe53cd
Florian Heuberger <fh@flogersoft.de>
2026-03-08 14:50:01 +0100

    feat: redirect to original url after login

--- app/middleware/auth.ts
+++ app/middleware/auth.ts
@@ -1,6 +1,8 @@
-export default defineNuxtRouteMiddleware(() => {
+export default defineNuxtRouteMiddleware((to) => {
   const { loggedIn } = useUserSession()
   if (!loggedIn.value) {
+    const redirect = useCookie('auth-redirect')
+    redirect.value = to.fullPath
     const localePath = useLocalePath()
     return navigateTo(localePath('/'))
   }
